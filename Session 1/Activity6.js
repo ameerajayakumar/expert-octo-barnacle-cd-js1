3426cde1688e946eb63db78a2b859ea64601f702
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-06-07 23:39:31 +0530

    session1 activity6

--- Session 1/Activity6.js
+++ Session 1/Activity6.js
@@ -1,6 +1,17 @@
-var a = 4;
-if (a === 4) {
-  console.log('Hello');
+// var a = 4;
+// if (a === 4) {
+//   console.log('Hello');
+// } else {
+//   console.log('World');
+// }
+
+var marks = 89;
+if (marks <= 40) {
+  console.log('Fail');
+} else if (marks <= 60) {
+  console.log('C');
+} else if (marks <= 80) {
+  console.log('B');
 } else {
-  console.log('World');
+  console.log('A');
 }
