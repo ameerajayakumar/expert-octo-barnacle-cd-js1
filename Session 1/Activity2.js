4af38ecee919944b4469cdc60b6794d4f61a73cd
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-06-07 21:42:16 +0530

    session1 activity2,3

---
+++ Session 1/Activity2.js
@@ -0,0 +1,8 @@
+console.log(2 + 2);
+console.log(5 * 15);
+console.log(10 % 2);
+console.log(19 - 29);
+console.log('Hello ' + 'World');
+console.log(3 + 10 * 2);
+console.log((3 + 10) * 2);
+console.log(12 / 'hello');
---
+++ Session 1/Activity3.js
@@ -0,0 +1,8 @@
+// var a =2;
+// var b=3;
+// var c="4";
+// console.log(a+b*c)
+var greeter = 'hey hi';
+console.log(greeter);
+var greeter = 'what happened today?';
+console.log(greeter);
