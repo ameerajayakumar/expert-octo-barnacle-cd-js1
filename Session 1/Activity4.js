b6820eb534b622eafece9440103e7b5a5cfd8116
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-06-07 22:12:34 +0530

    session1 activity4,5,6

---
+++ Session 1/Activity4.js
@@ -0,0 +1,8 @@
+// let greeting = "say hi";
+// console.log(greeting)
+// let greeting = "say hello";
+// console.log(greeting)
+
+const greeting = 'say hi';
+console.log(greeting);
+greeting = 'say hello';
---
+++ Session 1/Activity6.js
@@ -0,0 +1,6 @@
+var a = 4;
+if (a === 4) {
+  console.log('Hello');
+} else {
+  console.log('World');
+}
