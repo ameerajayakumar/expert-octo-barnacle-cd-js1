714f2185e69ac8f2033029fadd96b71230831583
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-06-09 22:45:34 +0530

    session 2 js-1 activity 5,6

--- Session 2/Activity5.js
+++ Session 2/Activity5.js
@@ -1,9 +1,16 @@
-console.log('Hello World');
-console.log(undefined);
-console.log(2);
-console.log('hello');
-console.log(typeof("hb"));
-console.log(2+4)
-console.log(null);
-console.log(true);
-console.log(false);
+let fName = 'Ameera';
+let lName = 'J';
+let fullName = fName + lName;
+console.log(fName.length);
+console.log('\n');
+console.log(lName.length);
+console.log('\\');
+console.log(fullName.length);
+let ini = fName[0] + '\t' + lName[0];
+console.log(ini);
+console.log('\r');
+console.log(ini);
+console.log(ini.indexOf('J'));
+console.log(fName.lastIndexOf('e'));
+console.log(fName.indexOf('e'));
+console.log(fName.substring(2, 4));
--- Session 2/Activity6.js
+++ Session 2/Activity6.js
@@ -1,9 +1,12 @@
-console.log('Hello World');
-console.log(undefined);
-console.log(2);
-console.log('hello');
-console.log(typeof("hb"));
-console.log(2+4)
-console.log(null);
-console.log(true);
-console.log(false);
+let fName = 'Ameera';
+let lName = 'J';
+let fullName = fName + lName;
+console.log(fName.length);
+console.log(lName.length);
+console.log(fullName.length);
+let ini = fName[0] + lName[0];
+console.log(ini);
+console.log(ini.indexOf('J'));
+console.log(fName.lastIndexOf('e'));
+console.log(fName.indexOf('e'));
+console.log(fName.substring(2, 4));
