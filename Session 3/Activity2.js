634ff84c178b97e3f34c57e63f0c4ac26dd24753
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-06-12 11:48:30 +0530

    session 3 js-1 activity 1,2

--- Session 3/Activity2.js
+++ Session 3/Activity2.js
@@ -3,6 +3,9 @@ const person = {
   jobTitle: 'Doctor',
   email: 'nm@dr.com',
   isVerified: false,
+  moreDescription: {
+    hasInsure: false,
+  },
 };
 console.log(`${person.name} ${person.isVerified}`);
 
@@ -12,3 +15,21 @@ person.firstName = 'Bissi';
 person.lastName = 'Nessi';
 
 console.log(person);
+
+//deconstructing an object
+const {
+  jobTitle,
+  email: em,
+  moreDescription: { hasInsure },
+} = person;
+/**
+ * person.name,person.email,person.moreDescription.hasInsure
+ */
+console.log(jobTitle);
+console.log(em);
+console.log(hasInsure); // nested object
+
+const newUser = person;
+console.log(newUser);
+newUser.name = 'Ameera';
+console.log(newUser);
